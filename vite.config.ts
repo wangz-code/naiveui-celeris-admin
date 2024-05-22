2ac042545114b38c164e34b3ac77a3769c412904
wangqz <2809755637@qq.com>
2024-05-22 14:36:44 +0800

    fix(build): gitpage 404

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,23 @@
 import { createViteConfig } from "./viteconfig/config/index";
+
+// eslint-disable-next-line no-control-regex
+const INVALID_CHAR_REGEX = /[\x00-\x1F\x7F<>*#"{}|^[\]`;?:&=+$,]/g;
+const DRIVE_LETTER_REGEX = /^[a-z]:/i;
+
 export default createViteConfig({
-	overrides: {},
+	overrides: {
+		build: {
+			rollupOptions: {
+				output: {
+					// https://github.com/rollup/rollup/blob/master/src/utils/sanitizeFileName.ts
+					sanitizeFileName(name) {
+						const match = DRIVE_LETTER_REGEX.exec(name);
+						const driveLetter = match ? match[0] : "";
+						// substr 是被淘汰語法，因此要改 slice
+						return driveLetter + name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, "");
+					},
+				},
+			},
+		},
+	},
 });
