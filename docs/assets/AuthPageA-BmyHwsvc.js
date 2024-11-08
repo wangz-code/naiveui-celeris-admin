d4d8ab5f9d49b0f84b65281c88128a9b482d0338
wangqz <2809755637@qq.com>
2024-11-08 15:32:42 +0800

    update pages

---
+++ docs/assets/AuthPageA-BmyHwsvc.js
@@ -0,0 +1,1 @@
+import{d as t,u as e,c as s,e as a,L as o,o as n,bz as r}from"./index-Chs2zBSz.js";const u=t({__name:"AuthPageA",setup(t){const{t:u}=e();return(t,e)=>{const l=o;return n(),s(l,{title:a(u)("page.permission.roleButtonText",{role:a(r).ADMIN})},null,8,["title"])}}});export{u as default};
---
+++ docs/assets/useLoading-Z-KTNKpg.js
@@ -0,0 +1,1 @@
+import{br as o}from"./index-Chs2zBSz.js";function n(n=!1){const[t,e]=o(n);return{loading:t,setLoading:o=>{t.value=o},toggle:e}}export{n as u};
