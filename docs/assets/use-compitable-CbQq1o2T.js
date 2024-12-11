9134a32229b32d652d4b998aa4cf1ce0f3143a97
wangqz <2809755637@qq.com>
2024-12-11 13:35:43 +0800

    fix bug

---
+++ docs/assets/use-compitable-CbQq1o2T.js
@@ -0,0 +1,1 @@
+import{l as r}from"./index-DMvMf093.js";function n(n,o){return r((()=>{for(const r of o)if(void 0!==n[r])return n[r];return n[o[o.length-1]]}))}export{n as u};
---
+++ docs/assets/useLoading-Da9i7NgS.js
@@ -0,0 +1,1 @@
+import{bt as o}from"./index-DMvMf093.js";function t(t=!1){const[n,e]=o(t);return{loading:n,setLoading:o=>{n.value=o},toggle:e}}export{t as u};
